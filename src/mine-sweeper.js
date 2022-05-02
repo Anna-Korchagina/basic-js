f939647cd1cd04746e1ccdfbd3fdbee17b363436
Anna-Korchagina <korchagina.anna.507@gmail.com>
2022-05-02 19:48:31 +0300

    feat: do minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,9 +25,100 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function minesweeper(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  const mines = [],
+    fieldLength = matrix[0].length,
+    matrixLength = matrix.length;
+  let minesCount = 0;
+
+  for (let i = 0; i < matrixLength; i++) {
+    let str = [];
+    for (let j = 0; j < fieldLength; j++) {
+      if (matrix[i][j] == true) {
+        str.push('mine');
+      } else {
+        str.push('nope');
+      }
+    }
+    mines.push(str);
+  }
+
+  for (let i = 0; i < matrixLength; i++) {
+    for (let j = 0; j < fieldLength; j++) {
+      if (mines[i][j] === 'nope') {
+        // findMines();
+
+        // find horizont:
+        if (j > 0) {
+          if (mines[i][j - 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+        if (j < (fieldLength - 1)) {
+          if (mines[i][j + 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+
+        // find vertical:
+        if (i > 0) {
+          if (mines[i - 1][j] === 'mine') {
+            minesCount += 1;
+          }
+        }
+        if (i < (matrixLength - 1)) {
+          if (mines[i + 1][j] === 'mine') {
+            minesCount += 1;
+          }
+        }
+
+        // find left diagonal:
+        if (i > 0 && j > 0) {
+          if (mines[i - 1][j - 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+        if ((i < (matrixLength - 1)) && (j > 0)) {
+          if (mines[i + 1][j - 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+
+        // find right diagonal:
+        if ((i > 0) && (j < (fieldLength - 1))) {
+          if (mines[i - 1][j + 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+        if ((i < (matrixLength - 1)) && (j < (fieldLength - 1))) {
+          if (mines[i + 1][j + 1] === 'mine') {
+            minesCount += 1;
+          }
+        }
+
+        mines[i][j] = minesCount;
+        minesCount = 0;
+
+      }
+
+
+    }
+  }
+
+  for (let i = 0; i < mines.length; i++) {
+    for (let j = 0; j < fieldLength; j++) {
+      if (mines[i][j] === 'mine') {
+        mines[i][j] = 1;
+      }
+    }
+  }
+
+
+
+  return mines;
 }
 
 module.exports = {
