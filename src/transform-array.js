d2d54e8480f3e9dbe3391648bc7a3a230021d1b1
Anna-Korchagina <korchagina.anna.507@gmail.com>
2022-05-02 17:32:00 +0300

    feat: do getSumOfDigits

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,9 +15,90 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(arr) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+  // if (!Array.isArray(arr)) {
+  //   return "'arr' parameter must be an instance of the Array!";
+  // }
+
+  // let transformArr = [];
+
+  // let currElem = null,
+  //   prevElem = null,
+  //   nextElem = null,
+  //   discardNext = false,
+  //   discardPrev = false,
+  //   doubleNext = false,
+  //   doublePrev = false;
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   currElem = arr[i];
+
+  //   if (i < arr.length - 1) {
+  //     nextElem = arr[i + 1];
+  //   } else {
+  //     nextElem = null;
+  //   }
+
+  //   if (i === 0) {
+  //     prevElem = null;
+  //     (arr.length === 1) ? nextElem = null: nextElem = arr[1];
+  //   }
+
+  //   if (discardNext) {
+  //     prevElem = arr[i];
+  //     discardNext = false;
+  //     continue;
+  //   }
+  //   if (doubleNext) {
+  //     prevElem = arr[i];
+  //     transformArr.push(arr[i]);
+  //     transformArr.push(arr[i]);
+  //     doubleNext = false;
+  //     continue;
+  //   }
+  //   if (discardPrev) {
+  //     if (transformArr.length === 0) {
+  //       discardPrev = false;
+  //     } else {
+  //       transformArr.pop();
+  //       discardPrev = false;
+  //     }
+  //   }
+  //   if (doublePrev) {
+  //     if (transformArr.length > 0) {
+  //       transformArr.push(transformArr[transformArr.length - 1]);
+  //       transformArr.push(arr[i]);
+  //       prevElem = arr[i];
+  //       doublePrev = false;
+  //       continue;
+  //     } else {
+  //       doublePrev = false;
+  //     }
+  //   }
+
+  //   if (arr[i] === '--discard-next') {
+  //     discardNext = true;
+  //   } else if (arr[i] === '--discard-prev') {
+  //     discardPrev = true;
+  //     if (i === arr.length - 1) {
+  //       transformArr.pop();
+  //     }
+  //   } else if (arr[i] === '--double-next') {
+  //     doubleNext = true;
+  //   } else if (arr[i] === '--double-prev') {
+  //     doublePrev = true;
+  //     if (i === arr.length - 1) {
+  //       transformArr.push(transformArr[transformArr.length - 1]);
+  //     }
+  //   } else {
+  //     transformArr.push(arr[i]);
+  //   }
+  //   prevElem = arr[i];
+  // }
+
+  // return transformArr;
 }
 
 module.exports = {
